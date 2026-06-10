669e11d25f45f3a93ad11cf70184ef67b88f8c9b
St Michael Egenamba <michaelegenamba@gmail.com>
2026-06-10 06:46:10 -0700

    Finished

--- app.py
+++ app.py
@@ -7,19 +7,32 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    pass  # TODO: Return a welcome message
+    return "<h1>Welcome<h1>"
 
 # TODO: Implement GET /products route that returns all products or filters by category
 
 @app.route("/products")
 def get_products():
-    pass  # TODO: Return all products or filter by ?category=
+     # Return all products or filter by ?category=
+    category_filter = request.args.get("category")
+    if category_filter:
+        filtered = [p for p in products if p["category"] == category_filter]
+        return jsonify(filtered)
+    return jsonify(products)
+
+   
 
 # TODO: Implement GET /products/<id> route that returns a specific product by ID or 404
 
 @app.route("/products/<int:id>")
 def get_product_by_id(id):
-    pass  # TODO: Return product by ID or 404
+    # Return product by ID or 404
+    for p in products:
+        if p['id'] == id:
+            return jsonify(p)
+    return jsonify({"error": "Products not found"}), 404
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
